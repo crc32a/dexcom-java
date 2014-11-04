6568a73bf84ceff7d1d3d3fa3fc7806ae9855c43
Carlos D. Garza <crc32@att.net>
2014-11-03 19:44:51 -0600

    Using or expressions instead of additions in BitPacker.

--- dexcom-reader/src/main/java/org/cgarza/exp/utils/BitPacker.java
+++ dexcom-reader/src/main/java/org/cgarza/exp/utils/BitPacker.java
@@ -1,7 +1,5 @@
 package org.cgarza.exp.utils;
 
-import java.nio.ByteBuffer;
-
 public class BitPacker {
 
     public static int ubyte2int(byte in) {
@@ -14,12 +12,12 @@ public class BitPacker {
     }
 
     public static int ushortLE2Int(byte[] data, int idx) {
-        return ubyte2int(data[idx]) + ubyte2int(data[idx + 1]) << 8;
+        return ubyte2int(data[idx]) | ubyte2int(data[idx + 1]) << 8;
     }
 
     public static long uintLE2long(byte[] data, int idx) {
-        return ubyte2int(data[idx]) + ubyte2int(data[idx + 1]) << 8
-                + ubyte2int(data[idx + 2]) << 16 + ubyte2int(data[idx + 3]) << 24;
+        return ubyte2int(data[idx]) | ubyte2int(data[idx + 1]) << 8
+                | ubyte2int(data[idx + 2]) << 16 | ubyte2int(data[idx + 3]) << 24;
     }
 
     public static int intLE2int(byte[] data, int idx) {
